bc1d90143d408db364088cbbb92d79b9e5d17e41
John Daybell <46499091+Oakwhisper@users.noreply.github.com>
2023-12-02 20:38:56 -0500

    Made it so input was an array of strings

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Index() {
-  return (
+  return (<>
     <div>Welcome to Advent of Code 2023</div>
+    <Link href="/Day 1">Go to Day 1</Link>
+    <div> test </div>
+  </>
   )
 }
