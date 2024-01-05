9b46f2aba2beaa7d5ee671622482b2686677b905
John Daybell <46499091+Oakwhisper@users.noreply.github.com>
2024-01-05 11:02:42 -0800

    Now actually smooths

--- app/leetCode/661/imageSmoother.test.ts
+++ app/leetCode/661/imageSmoother.test.ts
@@ -1,22 +1,78 @@
-import imageSmoother from "./imageSmoother";
+import imageSmoother, {getAverage} from "./imageSmoother";
 
 describe('ImageSmoother', ()=>{
-    it('should get average of center point', ()=>{
-        const testMatrix = [[0,0,0],[0,27,0],[0,0,0]]
+    describe('getAverage', ()=>{
+        it('should get average of center point', ()=>{
+            const testPoint = {row: 1, column: 1}
+            const testMatrix = [[0,0,0],[0,27,0],[0,0,0]]
 
-        const expected = [[0,0,0],[0,3,0],[0,0,0]]
+            const expected = 3
 
-        const actual = imageSmoother(testMatrix)
+            const actual = getAverage(testPoint, testMatrix)
 
-        expect(actual).toEqual(expected)
+            expect(actual).toEqual(expected)
+        })
+        it('should round down', ()=>{
+            const testPoint = {row: 1, column: 1}
+            const testMatrix = [[0,0,0],[0,26,0],[0,0,0]]
+
+            const expected = 2
+
+            const actual = getAverage(testPoint, testMatrix)
+
+            expect(actual).toEqual(expected)
+        })
+        it( 'should handle point on first row', ()=>{
+            const testPoint = {row: 0, column: 1}
+            const testMatrix = [[0,0,0],[0,26,0],[0,0,0]]
+
+            const expected = 4
+
+            const actual = getAverage(testPoint, testMatrix)
+
+            expect(actual).toEqual(expected)
+        })
+        it( 'should handle point on last row', ()=>{
+            const testPoint = {row: 2, column: 1}
+            const testMatrix = [[0,0,0],[0,26,0],[0,0,0]]
+
+            const expected = 4
+
+            const actual = getAverage(testPoint, testMatrix)
+
+            expect(actual).toEqual(expected)
+        })
+        it( 'should handle point on first column', ()=>{
+            const testPoint = {row: 1, column: 0}
+            const testMatrix = [[0,0,0],[0,26,0],[0,0,0]]
+
+            const expected = 4
+
+            const actual = getAverage(testPoint, testMatrix)
+
+            expect(actual).toEqual(expected)
+        })
+        it( 'should handle point on last column', ()=>{
+            const testPoint = {row: 1, column: 2}
+            const testMatrix = [[0,0,0],[0,26,0],[0,0,0]]
+
+            const expected = 4
+
+            const actual = getAverage(testPoint, testMatrix)
+
+            expect(actual).toEqual(expected)
+        })
     })
-    it('should round down', ()=>{
-        const testMatrix = [[0,0,0],[0,26,0],[0,0,0]]
+    describe('imageSmoother', ()=>{
+        it('should handle more complex grids', ()=>{
+            const testMatrix = [[100,200,100],[200,50,200],[100,200,100]]
 
-        const expected = [[0,0,0],[0,2,0],[0,0,0]]
+            const expected = [[137,141,137],[141,138,141],[137,141,137]]
 
-        const actual = imageSmoother(testMatrix)
+            const actual = imageSmoother(testMatrix)
 
-        expect(actual).toEqual(expected)
+            expect(actual).toEqual(expected)
+        })
     })
+
 })
--- app/leetCode/661/imageSmoother.ts
+++ app/leetCode/661/imageSmoother.ts
@@ -1,19 +1,33 @@
 export default function imageSmoother(img: number[][]): number[][] {
-    let newimg = [[0, 0, 0], [0, getAverage({row: 1, column: 1}, img), 0], [0, 0, 0]]
+    let newimg = []
+
+    for (let i = 0; i<img.length; i++){
+        let newRow = []
+        for (let j = 0; j<img[0].length; j++) {
+            newRow.push(getAverage({row: i, column: j}, img))
+        }
+        newimg.push(newRow)
+    }
 
     return newimg
 }
 
-function getAverage(point: Point, image: number[][]) {
+export function getAverage(point: Point, image: number[][]) {
     let sum = 0
+    let pointsConsidered = 0
 
     for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-            sum += image[point.row+i][point.column-j]
+        if (point.row+i>=0 && point.row+i<image.length) {
+            for (let j = -1; j <= 1; j++) {
+                if (point.column+j>=0&& point.column+j<image[0].length) {
+                    sum += image[point.row + i][point.column + j]
+                    pointsConsidered++
+                }
+            }
         }
     }
 
-    return Math.floor(sum/9)
+    return Math.floor(sum/pointsConsidered)
 }
 
 type Point = {
